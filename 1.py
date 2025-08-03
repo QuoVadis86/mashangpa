5723a523fa1c175255e1b0910a40d45a9b44a20e
QuoVadis <86292776+QuoVadis86@users.noreply.github.com>
2025-08-04 07:41:25 +0800

    格式化1.py

--- 1.py
+++ 1.py
@@ -1,17 +1,14 @@
 import requests
-
-# headers={
-#     # "Content-Type": "application/json",
-#     "cookie":"sessionid=ujb90uxi0132jydmceljxiu2emt7hdpp"
+url="https://mashangpa.com/api/problem-detail/1/data/
+# headers={}  "cookie":"sessionid=ujb90uxi0132jydmceljxiu2emt7hdpp"
 # }
-cookie ={
-    "sessionid":"ujb90uxi0132jydmceljxiu2emt7hdpp"
+cookie ={ "sessionid":"ujb90uxi0132jydmceljxiu2emt7hdpp"
 }
 
 result=[]
 for i in range(1,21):
-    url=f"https://mashangpa.com/api/problem-detail/1/data/?page={i}"
-    res=requests.get(url,cookies=cookie)
+    params = {"page":i}
+              res=requests.get(url,params=params,cookies=cookie)
     # print(res.json())
     result+= res.json()['current_array']
 print(sum(result))
